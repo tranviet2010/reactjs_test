77ca937312c90b845b05756c4408399383351bfd
Tran Van Viet <viettv@neo.vn>
2021-11-23 16:52:26 +0700

    push_code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
-  componentDidMount() {
-    var axios = require("axios");
-    var config = {
-      method: "post",
-      url: "https://10.252.12.19:9500/oauth2/token?grant_type=client_credentials",
-      headers: {
-        Authorization:
-          "Basic SV90cTBkMFVaMGVHN3RZMWpaaUxpV2tPcXNNYTpxTzN2TDFxSjhFdE5XR1NlZHNNZ3VpR1lZZ3Nh",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    };
-
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  constructor(props){
+    super(props)
+    this.state={
+      data:[
+        {id:1,kw:'TĐ. Mỹ lý - 38MW',time_run:'Dự kiến phát điện 2021'},
+        {id:2,kw:'TĐ. Nậm mô - 38MW',time_run:'Hoạt động từ 2013'},
+        {id:3,kw:'TĐ. Nậm nơn - 20MW',time_run:'Dự kiến phát điện 2014'},
+        {id:4,kw:'Thạch giám',time_run:'(H:1987 -nay)'},
+        {id:5,kw:'TĐ. Bản vẽ',time_run:'Hoạt động 2010'},
+      ],
+      text:'',
+    }
+  }
+  handgle=(a)=>{
+    this.setState({
+      text:a
+    })
+  }
+  someOtherHandler=()=>{
+    this.setState({
+      text:''
+    })
   }
   render() {
+    const {data,text}=this.state;
     return (
-      <div>
-        <p>controller</p>
+      <div className="all_main">
+          {data.map((val,key)=>(
+            <div key={key} className={`cusoor noname${val.id}`} onMouseOver={()=>this.handgle(val.id)} onMouseOut={()=>this.someOtherHandler()}>
+                  {text===val.id?<div className={`dis_block${val.id}`}>
+                        <span>{val.kw}</span><br />
+                        <span>{val.time_run}</span>
+                  </div>:''}
+            </div>
+          ))}
       </div>
     );
   }
